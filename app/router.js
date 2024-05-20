62636db8dcb883229d6c830e67665fed4e3dbc49
Yakov <yashapolezhaev@yandex.ru>
2024-05-20 22:58:15 +0500

    first commit

--- app/router.js
+++ app/router.js
@@ -7,6 +7,10 @@ const Router = EmberRouter.extend({
 });
 
 Router.map(function() {
+  this.route('meetings');
+  this.route('speakers');
+  this.route('books');
+  this.route('not-found', {path: '/*path'});
 });
 
 export default Router;
